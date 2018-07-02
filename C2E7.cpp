9cd64c9c3e8da3368518ed3630d2d0b7b45d546b
JM643 <40440123+JM643@users.noreply.github.com>
2018-07-02 19:22:31 -0400

    Create C2E7.cpp

---
+++ C2E7.cpp
@@ -0,0 +1,29 @@
+//
+//  main.cpp
+//  Ch2Ex7
+//
+//  Created by JM643 on 7/2/18.
+//  Copyright © 2018 JM643. All rights reserved.
+//
+
+#include <iostream>
+void time(int h, int m);
+
+int main()
+{
+    using namespace std;
+    cout << "Enter the number of hours:";
+    int h;
+    cin >> h;
+    cout << "Enter the number of minutes:";
+    int m;
+    cin >> m;
+    time(h,m);
+    return 0;
+}
+
+void time(int h, int m)
+{
+    using namespace std;
+    cout << "Time:" << h << ":" << m << endl;
+}
