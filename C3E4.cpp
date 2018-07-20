cc86c79e1326ab3311a2b1971a0a4dd2c360f13c
JM643 <40440123+JM643@users.noreply.github.com>
2018-07-20 16:52:26 -0400

    Create C3E4.cpp

---
+++ C3E4.cpp
@@ -0,0 +1,33 @@
+//
+//  main.cpp
+//  C3E4
+//
+//  Created by JM643 on 7/20/18.
+//  Copyright © 2018 JM643. All rights reserved.
+//
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    
+    const long sec = 60;
+    const long min = 60;
+    const long day = 24;
+    long seconds;
+    long minutes;
+    long hours;
+    long days;
+    long n_sec;
+    
+    cout << "Enter the number of seconds:";
+    cin >> seconds;
+    
+    days = seconds / 86400;
+    hours = seconds / 3600 % day;
+    minutes = seconds / sec % min;
+    n_sec = seconds % sec;
+   
+    cout << seconds <<" "  "seconds " " = " " " << days << " " "days," " " << hours <<" " "hours," " " << minutes << " " "minutes," " " << n_sec << " " "seconds" << endl;
+    return 0;
+}
