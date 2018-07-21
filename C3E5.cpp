8ded734dae229a97df8a32ae397809b9968aed48
JM643 <40440123+JM643@users.noreply.github.com>
2018-07-21 16:44:37 -0400

    Update C3E5.cpp
    Leaving variables as float. 
    
    At this point, I'm not sure that you can output long long variables with decimals. I'm pretty sure that you can't.

--- C3E5.cpp
+++ C3E5.cpp
@@ -10,7 +10,6 @@
 
 using namespace std;
 float w_pop, us_pop;
-double pert;
 
 int main()
 {
@@ -18,9 +17,6 @@ int main()
     cin >> w_pop;
     cout << "Enter the population of the US:" " ";
     cin >> us_pop;
-    
-    pert = us_pop/w_pop * 100;
-    
-    cout << "The population of the US is" " " << pert <<"%" " " "of the world population." << endl;
+    cout << "The population of the US is" " " << us_pop/w_pop * 100 <<"%" " of the world population." << endl;
     return 0;
 }
